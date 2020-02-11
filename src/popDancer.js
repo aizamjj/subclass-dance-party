036fa5f0a0aca9318fe562179615b988455cd541
aizamjj <aizamjig@gmail.com>
2020-02-11 12:32:59 -0800

    Created another dancer

---
+++ src/popDancer.js
@@ -0,0 +1,21 @@
+var MakePopDancer = function(top, left, timeBetweenSteps) {
+  MakeDancer.call(this, top, left, timeBetweenSteps);
+  this.$node = $('<span class="popDancer"></span>');
+
+};
+
+MakePopDancer.prototype = Object.create(MakeDancer.prototype);
+MakePopDancer.prototype.constructor = MakePopDancer;
+// MakePopDancer.prototype.secondStep = MakeDancer.prototype.step;
+// store the current step function at instance, which is superclass behavior
+
+// overwrite the step method to include toggle functionality
+MakePopDancer.prototype.step = function() {
+  // call the old version of step at the beginning of any call to this new version of step
+  MakeDancer.prototype.step.call(this);
+  // this.secondStep();
+  // toggle() is a jQuery method to show/hide the <span> tag.
+  // See http://api.jquery.com/category/effects/ for this and
+  // other effects you can use on a jQuery-wrapped html tag.
+  this.$node.toggle();
+};
