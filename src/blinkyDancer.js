a7ba58567db326a1bc2f50f89a1bb67a5a9adfe3
aizamjj <aizamjig@gmail.com>
2020-02-11 09:55:52 -0800

    Debug step method, call MakeDancer in the context of BlinkyDancer.

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -5,13 +5,14 @@ var MakeBlinkyDancer = function(top, left, timeBetweenSteps) {
 
 MakeBlinkyDancer.prototype = Object.create(MakeDancer.prototype);
 MakeBlinkyDancer.prototype.constructor = MakeBlinkyDancer;
-MakeBlinkyDancer.prototype.secondStep = MakeDancer.prototype.step;
+// MakeBlinkyDancer.prototype.secondStep = MakeDancer.prototype.step;
 // store the current step function at instance, which is superclass behavior
 
 // overwrite the step method to include toggle functionality
 MakeBlinkyDancer.prototype.step = function() {
   // call the old version of step at the beginning of any call to this new version of step
-  this.secondStep();
+  MakeDancer.prototype.step.call(this);
+  // this.secondStep();
   // toggle() is a jQuery method to show/hide the <span> tag.
   // See http://api.jquery.com/category/effects/ for this and
   // other effects you can use on a jQuery-wrapped html tag.
